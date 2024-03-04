1535a9c0d09f40ac845f8c1807e42da23a8118e1
zazedd <leomendesantos@gmail.com>
2024-03-04 22:23:40 +0000

    hardware.nix

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -8,6 +8,7 @@ keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOk8iAnIaa1deoc7jw8YACPNVka1ZFJxhn
     # ../../modules/vm/disk-config.nix
     ../../modules/shared
     ../../modules/shared/cachix
+    ./hardware.nix
     agenix.nixosModules.default
   ];
 
@@ -18,6 +19,7 @@ keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOk8iAnIaa1deoc7jw8YACPNVka1ZFJxhn
 
   networking.hostName = "nixvm"; 
   networking.networkmanager.enable = true;  
+  networking.useDHCP = true;
 
   time.timeZone = "Portugal";
 
---
+++ hosts/vm/hardware.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/profiles/qemu-guest.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "virtio_pci" "usbhid" "usb_storage" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/NIXROOT";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/NIXBOOT";
+      fsType = "vfat";
+    };
+
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+}
