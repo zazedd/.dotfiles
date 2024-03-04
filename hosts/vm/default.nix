116d9af3cf6b1eef0091a8aac7ea6149b6b3aac3
zazedd <leomendesantos@gmail.com>
2024-03-04 22:02:58 +0000

    another vm test

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -13,109 +13,111 @@ let user = "zazed";
 
   # Use the systemd-boot EFI boot loader.
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.device = "/dev/vda";
+  config = {
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.grub.device = "/dev/vda";
 
-  networking.hostName = "nixvm"; 
-  networking.networkmanager.enable = true;  
+    networking.hostName = "nixvm"; 
+    networking.networkmanager.enable = true;  
 
-  time.timeZone = "Portugal";
+    time.timeZone = "Portugal";
 
-  # Turn on flag for proprietary software
-  nix = {
-    # nixPath = [ "nixos-config=/home/${user}/.local/share/src/nixos-config:/etc/nixos" ];
-    settings.allowed-users = [ "${user}" ];
-    package = pkgs.nixUnstable;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-   };
-
-  # Manages keys and such
-  programs = {
-    gnupg.agent.enable = true;
-    zsh.enable = true;
-  };
-
-
-  services = {
-    xserver = {
-      enable = true;
-
-      # LightDM Display Manager
-      displayManager.defaultSession = "none+bspwm";
-      displayManager.lightdm = {
-        enable = true;
-        greeters.slick.enable = true;
+    # It's me, it's you, it's everyone
+    users.users = {
+      ${user} = {
+        isNormalUser = true;
+        extraGroups = [
+          "wheel" 
+        ];
+        shell = pkgs.zsh;
+        openssh.authorizedKeys.keys = keys;
+        home = "/home/${user}";
       };
 
-      # Tiling window manager
-      windowManager.bspwm = {
+      root = {
+        openssh.authorizedKeys.keys = keys;
+      };
+    };
+
+    # Turn on flag for proprietary software
+    nix = {
+      # nixPath = [ "nixos-config=/home/${user}/.local/share/src/nixos-config:/etc/nixos" ];
+      settings.allowed-users = [ "${user}" ];
+      package = pkgs.nixUnstable;
+      extraOptions = ''
+        experimental-features = nix-command flakes
+        '';
+    };
+
+    # Manages keys and such
+    programs = {
+      gnupg.agent.enable = true;
+      zsh.enable = true;
+    };
+
+
+    services = {
+      xserver = {
         enable = true;
+
+        # LightDM Display Manager
+        displayManager.defaultSession = "none+bspwm";
+        displayManager.lightdm = {
+          enable = true;
+          greeters.slick.enable = true;
+        };
+
+        # Tiling window manager
+        windowManager.bspwm = {
+          enable = true;
+        };
+
+        # Turn Caps Lock into Ctrl
+        layout = "us";
+        xkbOptions = "ctrl:nocaps";
+
+        # Better support for general peripherals
+        libinput.enable = true;
       };
 
-      # Turn Caps Lock into Ctrl
-      layout = "us";
-      xkbOptions = "ctrl:nocaps";
+      qemuGuest.enable = true;
+      spice-vdagentd.enable = true;
 
-      # Better support for general peripherals
-      libinput.enable = true;
+      # Let's be able to SSH into this machine
+      openssh.enable = true;
     };
 
-    qemuGuest.enable = true;
-    spice-vdagentd.enable = true;
+    # Don't require password for users in `wheel` group for these commands
+    # security.sudo = {
+    #   enable = true;
+    #   extraRules = [{
+    #     commands = [
+    #      {
+    #        command = "${pkgs.systemd}/bin/reboot";
+    #        options = [ "NOPASSWD" ];
+    #       }
+    #     ];
+    #     groups = [ "wheel" ];
+    #   }];
+    # };
 
-    # Let's be able to SSH into this machine
-    openssh.enable = true;
+    # fonts.packages = with pkgs; [
+    #   dejavu_fonts
+    #   emacs-all-the-icons-fonts
+    #   feather-font # from overlay
+    #   jetbrains-mono
+    #   font-awesome
+    #   noto-fonts
+    #   noto-fonts-emoji
+    # ];
+
+    environment.systemPackages = with pkgs; [
+      agenix.packages."${pkgs.system}".default # "x86_64-linux"
+        gitAndTools.gitFull
+        inetutils
+    ];
   };
-
-  # It's me, it's you, it's everyone
-  users.users = {
-    ${user} = {
-      isNormalUser = true;
-      extraGroups = [
-        "wheel" 
-      ];
-      shell = pkgs.zsh;
-      openssh.authorizedKeys.keys = keys;
-      home = "/home/${user}";
-    };
-
-    root = {
-      openssh.authorizedKeys.keys = keys;
-    };
-  };
-
-  # Don't require password for users in `wheel` group for these commands
-  # security.sudo = {
-  #   enable = true;
-  #   extraRules = [{
-  #     commands = [
-  #      {
-  #        command = "${pkgs.systemd}/bin/reboot";
-  #        options = [ "NOPASSWD" ];
-  #       }
-  #     ];
-  #     groups = [ "wheel" ];
-  #   }];
-  # };
-
-  # fonts.packages = with pkgs; [
-  #   dejavu_fonts
-  #   emacs-all-the-icons-fonts
-  #   feather-font # from overlay
-  #   jetbrains-mono
-  #   font-awesome
-  #   noto-fonts
-  #   noto-fonts-emoji
-  # ];
-
-  environment.systemPackages = with pkgs; [
-    agenix.packages."${pkgs.system}".default # "x86_64-linux"
-    gitAndTools.gitFull
-    inetutils
-  ];
 
   system.stateVersion = "21.05"; # Don't change this
 }
