15363ea2885e96a97f341ec3094b20bee28d8599
zazedd <leomendesantos@gmail.com>
2025-05-17 21:31:25 +0100

    ssl off for now

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -160,14 +160,13 @@ in
     enable = true;
     enableTCPIP = true;
     ensureDatabases = [ "firefly" ];
-    settings = {
-      ssl = true;
-    };
     authentication = pkgs.lib.mkOverride 10 ''
       #type database  DBuser  auth-method
       local all       all     trust
-      host  firefly    all     127.0.0.1/32 scram-sha-256
-      host  firefly    all     ::1/128 scram-sha-256
+      # ipv4
+      host  all      all     127.0.0.1/32   trust
+      # ipv6
+      host all       all     ::1/128        trust
     '';
     initialScript = pkgs.writeText "backend-initScript" ''
       CREATE ROLE firefly WITH LOGIN PASSWORD 'test' CREATEDB;
