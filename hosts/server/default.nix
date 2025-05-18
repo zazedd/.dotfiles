df50c64430126fa92ec0e463cc577d731ad3aa17
zazedd <leomendesantos@gmail.com>
2025-05-18 15:51:16 +0100

    test seafile

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -106,51 +106,24 @@ in
 
   services.minecraft-servers = import ./minecraft.nix { inherit pkgs; };
 
-  # services.ocis = {
-  #   enable = true;
-  #   address = "127.0.0.1";
-  #   port = 8384;
-  #   url = "http://0.0.0.0:8384";
-  #   environment = {
-  #     CS3_ALLOW_INSECURE = "true";
-  #     GATEWAY_STORAGE_USERS_MOUNT_ID = "123";
-  #     GRAPH_APPLICATION_ID = "1234";
-  #     IDM_IDPSVC_PASSWORD = "password";
-  #     IDM_REVASVC_PASSWORD = "password";
-  #     IDM_SVC_PASSWORD = "password";
-  #     IDP_ISS = "https://0.0.0.0:8385";
-  #     IDP_TLS = "false";
-  #     OCIS_INSECURE = "true";
-  #     OCIS_INSECURE_BACKENDS = "true";
-  #     OCIS_JWT_SECRET = "super_secret";
-  #     OCIS_LDAP_BIND_PASSWORD = "password";
-  #     OCIS_LOG_LEVEL = "error";
-  #     OCIS_MACHINE_AUTH_API_KEY = "foo";
-  #     OCIS_MOUNT_ID = "123";
-  #     OCIS_SERVICE_ACCOUNT_ID = "foo";
-  #     OCIS_SERVICE_ACCOUNT_SECRET = "foo";
-  #     OCIS_STORAGE_USERS_MOUNT_ID = "123";
-  #     OCIS_SYSTEM_USER_API_KEY = "foo";
-  #     OCIS_SYSTEM_USER_ID = "123";
-  #     OCIS_TRANSFER_SECRET = "foo";
-  #     STORAGE_USERS_MOUNT_ID = "123";
-  #     TLS_INSECURE = "true";
-  #     TLS_SKIP_VERIFY_CLIENT_CERT = "true";
-  #     WEBDAV_ALLOW_INSECURE = "true";
-  #   };
-  # };
+  services.seafile = {
+    enable = true;
+
+    adminEmail = "admin@example.com";
+    initialAdminPassword = "change this later!";
+
+    ccnetSettings.General.SERVICE_URL = "http://127.0.0.1:8384";
+  };
 
   services.nginx = {
     enable = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
     virtualHosts = {
-      "_" = {
-        # forceSSL = true;
-        # enableACME = true;
-        # locations."/".proxyPass = "http://127.0.0.1:8384";
-        default = true;
-        locations."/".return = "444";
+      "cloud.${domain}" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/".proxyPass = "http://127.0.0.1:8384";
       };
 
       "ff.${domain}" = {
