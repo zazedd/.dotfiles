b8e863089aa8b34957fb2104062949fcbee43934
zazedd <leomendesantos@gmail.com>
2025-05-19 11:30:52 +0100

    added media group

--- hosts/server/arr.nix
+++ hosts/server/arr.nix
@@ -3,13 +3,26 @@
 }:
 
 {
-  services.jellyfin.enable = true;
-  services.sonarr.enable = true;
-  services.radarr.enable = true;
+  users.groups.media = { };
+
+  services.jellyfin = {
+    enable = true;
+    group = "media";
+  };
+  services.sonarr = {
+    enable = true;
+    group = "media";
+  };
+  services.radarr = {
+    enable = true;
+    group = "media";
+  };
 
   services.prowlarr.enable = true;
+
   services.deluge = {
     enable = true;
+    group = "media";
     openFirewall = true;
     web = {
       enable = true;
@@ -17,9 +30,15 @@
       port = 5000;
     };
   };
-  services.sabnzbd.enable = true;
+  services.sabnzbd = {
+    enable = true;
+    group = "media";
+  };
   services.flaresolverr.enable = true;
 
   services.jellyseerr.enable = true;
-  services.bazarr.enable = true;
+  services.bazarr = {
+    enable = true;
+    group = "media";
+  };
 }
