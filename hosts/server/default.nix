5524e35506a2628cc07c108645a0f0eb32791b30
zazedd <leomendesantos@gmail.com>
2025-05-22 21:58:33 +0100

    fix seafile

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -168,8 +168,29 @@ in
             sslCertificate = "/var/lib/acme/ff.${domain}/fullchain.pem";
             sslCertificateKey = "/var/lib/acme/ff.${domain}/key.pem";
             locations = {
-              "/".proxyPass = "http://unix:/run/seahub/gunicorn.sock";
-              "/seafhttp".proxyPass = "http://127.0.0.1:8082/";
+              "/" = {
+                proxyPass = "http://unix:/run/seahub/gunicorn.sock";
+                extraConfig = ''
+                  proxy_set_header   Host $host;
+                  proxy_set_header   X-Real-IP $remote_addr;
+                  proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
+                  proxy_set_header   X-Forwarded-Host $server_name;
+                  proxy_read_timeout  1200s;
+                  client_max_body_size 0;
+                '';
+              };
+              "/seafhttp" = {
+                proxyPass = "http://127.0.0.1:8082/";
+                extraConfig = ''
+                  rewrite ^/seafhttp(.*)$ $1 break;
+                  client_max_body_size 0;
+                  proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
+                  proxy_connect_timeout  36000s;
+                  proxy_read_timeout  36000s;
+                  proxy_send_timeout  36000s;
+                  send_timeout  36000s;
+                '';
+              };
             };
           };
 
