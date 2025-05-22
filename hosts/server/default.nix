eb45629c26a6062cccc94c8ba109fc3bc0060e71
zazedd <leomendesantos@gmail.com>
2025-05-22 22:15:05 +0100

    test

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -171,7 +171,6 @@ in
               "/" = {
                 proxyPass = "http://unix:/run/seahub/gunicorn.sock";
                 extraConfig = ''
-                  proxy_set_header Host cloud.${domain};
                   proxy_set_header   X-Real-IP $remote_addr;
                   proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
                   proxy_set_header   X-Forwarded-Host $server_name;
