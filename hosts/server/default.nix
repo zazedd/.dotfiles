cb6ae3ced99440933e050fe6ed74fb835c65b241
zazedd <leomendesantos@gmail.com>
2025-05-31 00:13:19 +0100

    tmp

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -114,6 +114,7 @@ in
 
   services.nginx = import ./services/nginx.nix { inherit ports domain; };
 
+  my_nixpkgs.config.allowUnfree = true;
   services.factorio = import ./services/factorio.nix { inherit my_nixpkgs; };
 
   ## Rest
