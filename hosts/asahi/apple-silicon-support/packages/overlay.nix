668835189a43de61fc598910c76cdaaf7364c64a
Leonardo Santos <leomendesantos@gmail.com>
2026-03-24 21:47:48 +0000

    cleanup asahi, apps (#1)

--- hosts/asahi/apple-silicon-support/packages/overlay.nix
+++
@@ -1,7 +0,0 @@
-final: prev: {
-  linux-asahi = final.callPackage ./linux-asahi { };
-  uboot-asahi = final.callPackage ./uboot-asahi { };
-  asahi-fwextract = final.callPackage ./asahi-fwextract { };
-  alsa-ucm-conf-asahi = final.callPackage ./alsa-ucm-conf-asahi { inherit (prev) alsa-ucm-conf; };
-  asahi-audio = final.callPackage ./asahi-audio { };
-}
