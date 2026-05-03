7791b87b5bb6536a0624b70847c3a9c61d13a196
zazedd <leomendesantos@gmail.com>
2026-05-03 17:48:17 +0100

    readarr

--- modules/features/media-server/arr.nix
+++ modules/features/media-server/arr.nix
@@ -10,6 +10,7 @@ in
     services = {
       radarr = mediaDefaults;
       sonarr = mediaDefaults;
+      readarr = mediaDefaults;
       bazarr = mediaDefaults;
       prowlarr.enable = true;
       flaresolverr.enable = true;
@@ -20,6 +21,10 @@ in
     registry = {
       radarr.port = 7878;
       sonarr.port = 8989;
+      readarr = {
+        port = 8787;
+        aliases = [ "books" ];
+      };
       bazarr.port = 6767;
       prowlarr.port = 9696;
     };
